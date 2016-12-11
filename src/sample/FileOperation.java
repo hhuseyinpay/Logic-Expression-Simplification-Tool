cc30af0a1d7434d227de97304aa091be24d455d2
hhuseyinpay <h.huseyinpay@gmail.com>
2016-12-11 18:55:05 +0200

    update

--- src/sample/FileOperation.java
+++ src/sample/FileOperation.java
@@ -7,12 +7,12 @@ import java.io.*;
  */
 public class FileOperation {
 
-    private int[] functionRead(){
+    private int[] functionRead(String path) {
         return null;
     }
 
-    public int[] read(String directory) {
-        File folder = new File(directory);
+    public int[] read(String path) {
+        File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
         if (listOfFiles == null) {
             System.out.println("file not found");
@@ -20,9 +20,9 @@ public class FileOperation {
         }
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".be")) {
-                return functionRead(directory + listOfFiles[i].getName());
+                return functionRead(path + listOfFiles[i].getName());
             } else if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".tt")) {
-                return tableRead(directory + listOfFiles[i].getName());
+                return tableRead(path + listOfFiles[i].getName());
             }
         }
         return null;
@@ -30,7 +30,6 @@ public class FileOperation {
 
     private int[] tableRead(String path) {
 
-        String[] table;
         String line;
         try {
             InputStream fis = new FileInputStream(path);
@@ -42,44 +41,81 @@ public class FileOperation {
                 return null;
             }
 
-            int counter = 0;
+            int len = line.length();
+            int[] table = new int[len];
             try {
-                switch (line.length()) {
+                switch (len) {
                     case 5:
-
                         if (line.charAt(0) == 'A' && line.charAt(1) == ','
                                 && line.charAt(2) == 'B' && line.charAt(3) == ';'
                                 && line.charAt(4) == 'F') {
-
-                            while ((line = br.readLine()) != null) {
-
-
-                            }
-
-                            break;
+                            return checker(br, 2);
                         } else return null;
 
                     case 7:
-
+                        if (line.charAt(0) == 'A' && line.charAt(1) == ','
+                                && line.charAt(2) == 'B' && line.charAt(3) == ','
+                                && line.charAt(2) == 'C' && line.charAt(3) == ';'
+                                && line.charAt(4) == 'F') {
+                            return checker(br, 3);
+                        } else return null;
 
                     case 9:
+                        if (line.charAt(0) == 'A' && line.charAt(1) == ','
+                                && line.charAt(2) == 'B' && line.charAt(3) == ','
+                                && line.charAt(2) == 'C' && line.charAt(3) == ','
+                                && line.charAt(2) == 'D' && line.charAt(3) == ';'
+                                && line.charAt(4) == 'F') {
+                            return checker(br, 4);
+                        } else return null;
 
                     default:
                         return null;
                 }
-
-
             } catch (Exception e) {
+                System.out.println(e.toString());
                 return null;
             }
-        } catch (IOException ex) {
-            System.out.println(ex.toString());
+        } catch (
+                IOException e) {
+            System.out.println(e.toString());
             System.out.println("Error: FileOperation > tableRead()");
         }
-
         return null;
     }
 
 
+    private int[] checker(BufferedReader br, int len) {
+        try {
+            int counter;
+            int rows = (int) Math.pow(2, len); // stun sayısına göre satır sayısı
+            int[] table = new int[rows];
 
+            for (int i = 0; i < rows; i++) {//satırları dolaşıyor
+                String[] splited = br.readLine().split(";");
+                counter = 0;
+                if (splited[1].equals("1") || splited[1].equals("0")) {
+                    for (int j = len - 1; j >= 0; j--) {//stunları dolaşıyor
+                        if ((i / (int) Math.pow(2, j)) % 2 == Character.getNumericValue(splited[0].charAt(counter)))  //truth table'ın doğruluğu
+                            table[i] = Integer.parseInt(splited[1]);
+                        else return null;
+
+                        counter++;
+                        if (counter < len && splited[0].charAt(counter) != ',')
+                            return null;
+                        counter++;
+                    }
+                } else return null;
+            }
+
+            if (br.readLine() != null)
+                return null;
+
+            return table;
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.out.println("Error: FileOperation > checker()");
+            return null;
+        }
+    }
 }
